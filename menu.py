2f43f078c0c30640b605edc8976e6f60a429069f
Dan-Khai <64270594+Dan-Khai@users.noreply.github.com>
2020-04-24 18:27:49 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,1 +1,11 @@
 import algorithms
+while True:
+    rle = open("Logo-rle.txt", "a+")
+    art = open("logo-ASCII.txt", "a+")
+    algorithms.display_menu()
+    choice = input()
+    if choice == "A": print("\nHERE IS YOUR IMAGE:\n\n\n", algorithms.enter_rle())
+    elif choice == "B": algorithms.display_ASCII_art()
+    elif choice == "C": algorithms.convert_ASCII_art()
+    else:
+        break
